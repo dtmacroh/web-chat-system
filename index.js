8063c41b6557f3fcc44c13724431a1d93da1b9a3
dtmacroh@ucalgary.ca <dtmacroh@ucalgary.ca>
2018-03-02 22:07:02 -0700

    message history now displaying with user nick

--- index.js
+++ index.js
@@ -16,8 +16,17 @@ var clientArray = [];
 io.on('connection', function(socket){
     //initialization of client
     clientArray.push(socket.id);
-    console.log(socket.id);
+      
+    //var msgStore2 =msgStore;
+
+    // for(let i=0;i<msgStore.length;i++)
+    // {
+    //     msgStore2[i].clientId = mapping[msgStore[i].clientId];
+        
+    // }
     io.to(socket.id).emit('wel',msgStore);
+   
+
     io.to(socket.id).emit('nick', 'User'+ io.engine.clientsCount);
     mapping[socket.id] = 'User'+ io.engine.clientsCount;
     console.log("msgStore " +msgStore);
@@ -30,7 +39,7 @@ io.on('connection', function(socket){
         
         var msgObj = {time_id:time, body:msg,clientId:chatUsr};
         io.emit('chat',msgObj );
-        console.log("msgObj " +msgObj.time_id + " " + msgObj.body);
+        console.log("msgObj " +msgObj.time_id + " " + msgObj.body+ " "+msgObj.clientId);
         msgStore.push(msgObj);
         if (msgCount>=200){
             msgStore.shift();
@@ -44,12 +53,13 @@ io.on('connection', function(socket){
 
     socket.on('nick', function(nick){
         console.log(nick);
-        if (!nick in Object.values(mapping))
+        if (nick in Object.values(mapping))
         {
-            console.log("nick is granted");
+            console.log("nick in use");
         }
         else{
-            console.log("nick in use");
+            console.log("nick is granted");
+            mapping[socket.id] = nick;
         }
         io.to(socket.id).emit('nick', nick);
     //io.send('nick', nick);
--- public/client.js
+++ public/client.js
@@ -1,7 +1,7 @@
 // shorthand for $(document).ready(...)
 $(function() {
     var socket = io();
-    var userNickSt= "";
+    var userNickSt=  $('#userNick').value;
     socket.emit('wel', "hidi");
     $('form').submit(function(){
 
@@ -10,13 +10,11 @@ $(function() {
     var mess_args = message.split(" ");
     if (mess_args.length==2 && mess_args[0]=="/nick")
     {
-        console.log("changing Nick");
-       // sockets.socket(savedSocketId).emit()
+       
         socket.emit('nick', mess_args[1]);
         console.log(mess_args[1]);
     }
     else{
-        console.log("still of the chat");
         socket.emit('chat',message );
     }
     
@@ -31,23 +29,24 @@ $(function() {
         var body = msg.body;
     
         console.log(time + " "+ body);
-        $('#messages').append($('<li>').text( time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric' })+" "+msg.clientId+" " + msg.body));
+        $('#messages').append($('<li>').text( time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric' })+
+        " "+msg.clientId+" " + msg.body));
      });
 
     socket.on('nick', function(nick){
         
-       //console.log(nick);
+       //insert check here
         
        $('#userNick').text(nick);
         userNickSt =  $('#userNick').text();
     
     });
     socket.on('wel', function(wel){
-        console.log("i am welcomed");
         for ( var i=0; i< wel.length;i++)
         {
-            console.log("in client" +wel[i].time_id+ " "+wel[i].body)  ;
-            $('#messages').append($('<li>').text( (new Date(wel[i].time_id)).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric' })+" " + wel[i].body));
+            console.log("in client" +wel[i].time_id+ " "+wel[i].body+" "+wel[i].clientId)  ;
+            $('#messages').append($('<li>').text( (new Date(wel[i].time_id)).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric' })
+            +" " +wel[i].clientId+" " +wel[i].body));
 
         }
     });
